e014dd957c672e3b1fc324a6ed83767bfc9f0a3e
SasGyula <gyulasass43@gmail.com>
2024-03-14 16:08:28 +0100

    mindPrimE

--- src/tombosfeltoltes/TombosFeltoltes.java
+++ src/tombosfeltoltes/TombosFeltoltes.java
@@ -5,15 +5,25 @@ import java.util.Random;
 public class TombosFeltoltes {
     static Random rnd = new Random();
     public static void main(String[] args) {
-        int[] tombkiiras = tombfeltoltes(10);
+        primszamok();
+    }
+
+    private static void primszamok() {
+        int[] tombkiiras = tombfeltoltes(3);
         tombKiiras(tombkiiras);
-        primE(tombkiiras);
+        primekE(tombkiiras);
+    }
+    private static void primekE(int[] tombkiiras) {
+        System.out.println(primEOsszes(tombkiiras)? "Az összes prím":"Nem az összes prím");
     }
     private static int[] tombfeltoltes(int DB){
         int tomb[] = new int[DB];
         for (int i = 0; i < DB; i++) {
             tomb[i] = rnd.nextInt(100);
-        }        
+        }
+//        tomb[0] = 2;
+//        tomb[1] = 3;
+//        tomb[2] = 5;
         return tomb;
     }
     private static void tombKiiras(int[] tomb){
@@ -21,7 +31,7 @@ public class TombosFeltoltes {
             System.out.print(tomb[i]+" ");
         }
     }
-    private static void primE(int [] tomb){
+    private static int primE(int [] tomb){
         int db = 0;
         for (int i = 0; i < tomb.length; i++) {
             boolean primE;
@@ -43,5 +53,11 @@ public class TombosFeltoltes {
         }else{
             System.out.println("\nNincs benne prím!");
         }
+        return db;
+    }
+    private static boolean primEOsszes(int[] tomb){
+        int db = tomb.length;
+        int primekSzama = primE(tomb);
+        return db == primekSzama;
     }
 }
